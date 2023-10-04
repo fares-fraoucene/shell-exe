0c9b7fb88df3b2adf20270c32e56832c77fd0859
Fares <fares@Faressito>
2023-10-04 17:02:22 +0200

    Ajout du jobs8

--- Jobs8/get_logs.sh
+++ Jobs8/get_logs.sh
@@ -1,6 +1,12 @@
-#!/bin/bash
+#!/usr/bin/bash
 
-dateRecup=($date +"
+dateRecup=$(date +"%d-%m-%Y-%H:%M")
+logDirectory="/var/log/auth.log"
+fichier="number_connection-$dateRecup"
 
+grep "session opened for user fares" $logDirectory | wc -l > /mnt/c/Users/Fafad/Documents/GitHubRepostiry/shell-exe/Jobs8/number_connection-$dateRecup
 
-number_connection-Date
+tar -cf /mnt/c/Users/Fafad/Documents/GitHubRepostiry/shell-exe/Jobs8/number_connection-$dateRecup.tar $fichier
+mv /mnt/c/Users/Fafad/Documents/GitHubRepostiry/shell-exe/Jobs8/number_connection-$dateRecup.tar /mnt/c/Users/Fafad/Documents/GitHubRepostiry/shell-exe/Jobs8/Backups
+
+rm /mnt/c/Users/Fafad/Documents/GitHubRepostiry/shell-exe/Jobs8/number_connection-$dateRecup
